48d3003153e46062e0fd76598b90ce594512bf43
Justin Nguyen <justinhnguyen88@gmail.com>
2019-10-15 14:08:06 -0700

    CRUD complete

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,4 @@
+import history from '../history';
 import { 
     SIGN_IN, 
     SIGN_OUT, 
@@ -23,9 +24,11 @@ export const signOut = () => {
 };
 
 export const createStream = (formValues) => {
-    return async (dispatch) => {
-        const response = await streams.post('/streams', formValues);
+    return async (dispatch, getState) => {
+        const { userId } = getState().auth;
+        const response = await streams.post('/streams', {...formValues, userId});
         dispatch({ type: CREATE_STREAM, payload: response.data })
+        history.push('/')
     }
 };
 
@@ -45,8 +48,9 @@ export const fetchStream = (id) => {
 
 export const editStream = (id, formValues) => {
     return async (dispatch) => {
-        const response = await streams.put('/streams/' + id, formValues);
-        dispatch({ type: EDIT_STREAM, payload: response.data })
+        const response = await streams.patch('/streams/' + id, formValues);
+        dispatch({ type: EDIT_STREAM, payload: response.data });
+        history.push('/');
     }
 };
 
@@ -54,5 +58,7 @@ export const deleteStream = (id) => {
     return async (dispatch) => {
         await streams.delete('/streams/' + id)
         dispatch({ type: DELETE_STREAM, payload: id })
+        //Reroute
+        history.push('/')
     }
 };
--- client/src/components/streams/StreamEdit.js
+++ client/src/components/streams/StreamEdit.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { fetchStream, editStream } from '../../actions';
+import StreamForm from './StreamForm';
 
-const StreamEdit = () => {
-    return <div>StreamEdit</div>
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  onSubmit = (formValues) => {
+      this.props.editStream(this.props.match.params.id, formValues)
+  }
+
+  render(){
+        if(!this.props.stream){
+            return <div>Loading...</div>
+        }
+        return(
+            <div>
+                <h3>Edit A Stream</h3>
+                <StreamForm 
+                    initialValues={{ title:this.props.stream.title, description: this.props.stream.description }}
+                    onSubmit={this.onSubmit} 
+                />
+            </div>
+        )
+    }
+}
+
+// ownProps is provided by Connect, it is the Props obj
+// inside of the component
+const mapStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default StreamEdit;
+export default connect(
+  mapStateToProps,
+  { fetchStream, editStream }
+)(StreamEdit);
